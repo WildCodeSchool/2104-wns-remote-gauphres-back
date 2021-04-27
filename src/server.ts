682426b929295b1ec88b459e885d97ade9a74ad0
Benoit Deligny <benoit.deligny@manomano.com>
2021-04-27 16:41:46 +0200

    feat(global): app initialization

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import {graphqlHTTP} from 'express-graphql';
+import {buildSchema} from 'graphql';
+
+// GraphQL schema
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+// Root resolver
+const root = {
+    message: () => 'Hello World!'
+};
+
+// Create an express server and a GraphQL endpoint
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(5000, () => console.log('Express GraphQL Server Now Running On localhost:5000/graphql'));

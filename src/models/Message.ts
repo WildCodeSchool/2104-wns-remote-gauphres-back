7bdfcbfc3e3b9fbc75d799ed69d1f102bb2efb45
Benoit Deligny <benoit.deligny@manomano.com>
2021-04-28 21:13:35 +0200

    feat(message): remove constructor and add input version

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from 'type-graphql';
+import { Field, InputType, ObjectType } from 'type-graphql';
 import { User } from './User';
 
 @ObjectType()
@@ -11,4 +11,13 @@ export class Message {
 
     @Field()
     creationDate!: Date;
+}
+
+@InputType()
+export class CreateMessageInput {
+    @Field()
+    text!: string;
+
+    @Field()
+    creationDate: Date = new Date(Date.now());
 }

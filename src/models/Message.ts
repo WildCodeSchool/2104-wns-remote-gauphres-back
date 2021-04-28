fa26167c4524be2172ad7930b26b4236bb91c7ac
Benoit Deligny <benoit.deligny@manomano.com>
2021-04-28 20:07:47 +0200

    feat(Message): some basic fields added

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,4 +1,14 @@
-import { ObjectType } from 'type-graphql';
+import { Field, ObjectType } from 'type-graphql';
+import { User } from './User';
 
 @ObjectType()
-export class Message {}
+export class Message {
+    @Field()
+    text!: string;
+
+    @Field((type) => User)
+    author!: User;
+
+    @Field()
+    creationDate!: Date;
+}

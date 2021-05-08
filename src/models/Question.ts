c7d736dac9942c752960e959d7178f33f8b5e81a
Benoit Deligny <benoit.deligny@manomano.com>
2021-05-08 16:47:58 +0200

    feat(Models): add Question class

---
+++ src/models/Question.ts
@@ -0,0 +1,23 @@
+import { Prop } from "@typegoose/typegoose";
+import { ObjectType, Field } from "type-graphql";
+import { Hobby } from "./Hobby";
+
+
+@ObjectType()
+export class Question {
+@Prop()
+@Field()
+id?: string
+
+@Prop()
+@Field()
+texte?: string
+
+@Prop()
+@Field()
+image?: string
+
+@Prop({ type: Hobby })
+@Field((type) => [Hobby])
+hobbie?: Hobby[];
+}

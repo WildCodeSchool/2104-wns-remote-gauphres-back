c88ab6c8506296009c82cea97787d62269290b01
Benoit Deligny <benoit.deligny@manomano.com>
2021-05-08 16:47:46 +0200

    feat(Models): add Hobby class

---
+++ src/models/Hobby.ts
@@ -0,0 +1,26 @@
+import { Prop } from "@typegoose/typegoose";
+import { ObjectType, Field } from "type-graphql";
+import { User } from './User'
+
+@ObjectType()
+export class Hobby {
+@Prop()
+@Field()
+id?: string
+
+@Prop()
+@Field()
+title?: string
+
+@Prop({ type: User })
+@Field((type) => [User])
+users?: User[];
+
+@Prop()
+@Field()
+image?: string
+
+@Prop()
+@Field()
+category?: string
+}
